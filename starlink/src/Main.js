9ffcb1af0a39705ce87a219b867391f07fd9a553
Siping Wang <wangsiping1997@gmail.com>
2020-11-03 05:03:19 +0800

    create and add Main

---
+++ starlink/src/Main.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="main">
+                <div className="left">
+                    left
+                </div>
+                <div className="right">
+                    right
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
